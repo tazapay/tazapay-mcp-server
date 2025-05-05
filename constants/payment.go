6a78952a192b9ac4240e3e79ddb4583ce4299d98
Suyash <suyash@tazapay.com>
2025-05-05 18:07:02 +0530

    refactor: Update payment link and FX base URLs to use environment-specific constants

--- constants/payment.go
+++ constants/payment.go
@@ -1,10 +1,23 @@
 package constants
 
-// Payment constants
-const PaymentLinkBaseURL = "https://api-orange.tazapay.com/v3/checkout"
-const PaymentPayoutFxBaseURL = "https://service.tazapay.com/v3/fx/payout"
+// Payment Tools constants based on env
+
+// prod
+const PaymentLinkBaseURL_prod = "https://service.tazapay.com/v3/checkout"
+const PaymentFxBaseURL_prod = "https://service.tazapay.com/v3/fx/payout"
+//orange 
+const PaymentLinkBaseURL_orange = "https://api-orange.tazapay.com/v3/checkout"
+const PaymentFxBaseURL_orange = "https://api-orange.tazapay.com/v3/fx/payout"
+//purple 
+const PaymentLinkBaseURL_purple = "https://api-purple.tazapay.com/v3/checkout"
+const PaymentFxBaseURL_purple = "https://api-purple.tazapay.com/v3/fx/payout"
+//brown 
+const PaymentLinkBaseURL_brown = "https://api-brown.tazapay.com/v3/checkout"
+const PaymentFxBaseURL_brown = "https://api-brown.tazapay.com/v3/fx/payout"
+//yellow 
+const PaymentLinkBaseURL_yellow = "https://api-yellow.tazapay.com/v3/checkout"
+const PaymentFxBaseURL_yellow = "https://api-yellow.tazapay.com/v3/fx/payout"
 
 
-// String Constants
-
+// String Placeholder Constants
 const Miscellaneous = "Miscellaneous "
