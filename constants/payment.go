42718c13b48783038473243d2e93c4d139801c07
Suyash <suyash@tazapay.com>
2025-05-13 18:32:58 +0530

    refactor: Remove unused base URLs for testing environments

--- constants/payment.go
+++ constants/payment.go
@@ -4,18 +4,6 @@ package constants
 const (
 	// Production
 	ProdBaseURL = "https://service.tazapay.com/v3"
-
-	// Orange
-	OrangeBaseURL = "https://api-orange.tazapay.com/v3"
-
-	// Purple
-	PurpleBaseURL = "https://api-purple.tazapay.com/v3"
-
-	// Brown
-	BrownBaseURL = "https://api-brown.tazapay.com/v3"
-
-	// Yellow
-	YellowBaseURL = "https://api-yellow.tazapay.com/v3"
 )
 
 // API Path Segments
@@ -30,34 +18,6 @@ const (
 	PaymentLinkBaseURLProd = ProdBaseURL + CheckoutPath
 	PaymentFxBaseURLProd   = ProdBaseURL + FxPayoutPath
 	BalanceBaseURLProd     = ProdBaseURL + BalancePath
-)
-
-// Orange  URLs
-const (
-	PaymentLinkBaseURLOrange = OrangeBaseURL + CheckoutPath
-	PaymentFxBaseURLOrange   = OrangeBaseURL + FxPayoutPath
-	BalanceBaseURLOrange     = OrangeBaseURL + BalancePath
-)
-
-// Purple  URLs
-const (
-	PaymentLinkBaseURLPurple = PurpleBaseURL + CheckoutPath
-	PaymentFxBaseURLPurple   = PurpleBaseURL + FxPayoutPath
-	BalanceBaseURLPurple     = PurpleBaseURL + BalancePath
-)
-
-// Brown  URLs
-const (
-	PaymentLinkBaseURLBrown = BrownBaseURL + CheckoutPath
-	PaymentFxBaseURLBrown   = BrownBaseURL + FxPayoutPath
-	BalanceBaseURLBrown     = BrownBaseURL + BalancePath
-)
-
-// Yellow  URLs
-const (
-	PaymentLinkBaseURLYellow = YellowBaseURL + CheckoutPath
-	PaymentFxBaseURLYellow   = YellowBaseURL + FxPayoutPath
-	BalanceBaseURLYellow     = YellowBaseURL + BalancePath
 )
 
 // HTTP Method Constants
